b688afb69224113a5aef4ad01690f6448d817b13
admin <admin@artyoms-mbp.lan>
2022-12-21 16:47:27 +0400

    optimize inventory_current calculation

--- models/marts/inventory/inventory_current.sql
+++ models/marts/inventory/inventory_current.sql
@@ -15,26 +15,42 @@ products_with_details as (
     select * from {{ ref('int_products_with_details') }}
 ),
 
+offices as (
+    select * from {{ ref('stg_io__offices') }}
+),
+
+grouped as (
+    SELECT
+        comp_id,
+        domain_prefix,
+        office_id,
+        product_id,
+        sum(inventory_turnover) as inventory_current
+    FROM inventory_daily
+    GROUP BY 1, 2, 3, 4
+),
+
 final as (
-select
-    i.comp_id,
-    i.domain_prefix,
-    i.office_id,
-    i.product_id,
-    p.brand_id,
-    p.prod_name,
-    p.brand_name,
-    p.direct_category,
-    p.parent_category,
-    p.sub_category_1,
-    p.sub_category_2,
-    any_value(office_name) as office_name,
-    sum(inventory_turnover) as inventory_current
-from inventory_daily i
-left join products_with_details p 
-    on i.comp_id = p.comp_id
-    and i.product_id = p.prod_id
-group by 1,2,3,4,5,6,7,8,9,10,11
+    SELECT
+        grouped.comp_id,
+        grouped.domain_prefix,
+        grouped.office_id,
+        offices.office_name,
+        grouped.product_id,
+        p.brand_id,
+        p.prod_name,
+        p.brand_name,
+        p.direct_category,
+        p.parent_category,
+        p.sub_category_1,
+        p.sub_category_2,
+        grouped.inventory_current
+    FROM grouped
+    LEFT JOIN offices
+        on offices.office_id = grouped.office_id
+    LEFT JOIN products_with_details p 
+        on grouped.comp_id = p.comp_id
+        and grouped.product_id = p.prod_id
 )
 
 SELECT * FROM final
