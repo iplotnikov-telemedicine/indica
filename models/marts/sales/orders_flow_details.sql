d0e887396ccc598df186855b544c668bfe090b79
“Simarglok” <simarglok@gmail.com>
2023-03-07 17:46:49 +0400

    LOOK-159 added models for orders_flow_details mart

---
+++ models/marts/sales/orders_flow_details.sql
@@ -0,0 +1,49 @@
+{{
+    config(
+        materialized='view',
+        incremental_strategy='delete+insert',
+        unique_key=['comp_id', 'order_id'],
+        sort=['comp_id', 'order_id'],
+        dist='max_order_timestamp'
+    )
+}}
+
+
+{% set wol_types = [
+    "init",
+	"pending",
+	"accepted",
+	"declined",
+	"assigned",
+	"not_home",
+	"canceled",
+	"delivered",
+	"completed",
+	"returned",
+	"reassigned",
+	"fulfilling_started",
+	"fulfilling_stopped",
+	"fulfilled",
+	"refunded",
+	"shipped",
+	"fulfilling_timeout",
+	"delivery_started",
+	"delivery_stopped",
+	"rejected",
+	"delivery_accepted",
+	"exchanged"
+] %}
+
+
+select 
+	comp_id,
+	order_id,
+	max(created_at) as max_order_timestamp,
+	{% for wol_type in wol_types %}
+	max(case when type_name = '{{wol_type}}' then created_at end) as {{wol_type}}_at,
+	max(case when type_name = '{{wol_type}}' then sf_guard_user_name end) as {{wol_type}}_by,
+	max(case when type_name = '{{wol_type}}' then sf_guard_user_id end) as {{wol_type}}_by_id
+	{% if not loop.last %},{% endif %}
+	{% endfor %}
+from {{ ref('int_orders_flow_details') }}
+group by 1, 2
